949485d34de36b0c771539163bf03446049be750
Jerahmeel Cosinas <contact@jerahmeelcosinas.net>
2022-09-28 01:07:14 +1000

    typo

--- functions/api/ledger/index.js
+++ functions/api/ledger/index.js
@@ -1,6 +1,6 @@
 export const onRequestPost = async ({ env }) => {
   const id = env.EB_LEDGER_DO.newUniqueId();
-  var ledgerList = env.eb.get(`user:${data.auth.sub}:ledgers`, { type: "json" }) || [];
+  var ledgerList = env.EB.get(`user:${data.auth.sub}:ledgers`, { type: "json" }) || [];
   ledgerList.push(id);
   env.EB.put(`user:${data.auth.sub}:ledgers`, ledgerList);
   return new Response(id.toString(), { status: 201});
