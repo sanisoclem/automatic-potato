e0ea96023704242496c568304ed982a6e06fc52a
Jerahmeel Cosinas <contact@jerahmeelcosinas.net>
2022-09-28 00:12:12 +1000

    convert to middleware

---
+++ functions/api/_middleware.js
@@ -0,0 +1,60 @@
+import * as jose from "jose";
+
+const getCookie = (cookieString, key) => {
+  if (cookieString) {
+    const allCookies = cookieString.split("; ");
+    const targetCookie = allCookies.find((cookie) => cookie.includes(key));
+    if (targetCookie) {
+      const [_, value] = targetCookie.split("=");
+      return value;
+    }
+  }
+
+  return null;
+};
+
+export const onRequest = [
+  async (context) => {
+    context.data.timestamp = Date.now();
+    const res = await context.next();
+    let delta = Date.now() - context.data.timestamp;
+    res.headers.set("x-response-timing", delta);
+    return res;
+  },
+  async (context) => {
+    try {
+      return await context.next();
+    } catch (err) {
+      return new Response(`${err.message}\n${err.stack}`, { status: 500 });
+    }
+  },
+  async ({ request, next, data, env }) => {
+    var url = new URL(request.url);
+
+    if (!url.pathname.startsWith("api/auth/")) {
+      try {
+        const cookies = request.headers.get("Cookie");
+        const token = getCookie(cookies, "authToken");
+
+        if (!token) {
+          return new Response("Unauthorized", { status: 401 });
+        }
+
+        const { payload } = await jose.jwtVerify(
+          token,
+          await jose.importSPKI(env.AUTH_JWT_PUBLIC_KEY, "ES384"),
+          {
+            issuer: env.AUTH_JWT_ISSUER,
+            audience: env.AUTH_JWT_AUDIENCE,
+          }
+        );
+
+        data.auth = payload;
+      } catch {
+        return new Response("Unauthorized", { status: 403 });
+      }
+    }
+
+    return await next();
+  },
+];
--- functions/api/session.js
+++ functions/api/session.js
@@ -1,35 +1,3 @@
-import * as jose from 'jose'
-
-const getCookie = (cookieString, key) => {
-  if (cookieString) {
-    const allCookies = cookieString.split("; ")
-    const targetCookie = allCookies.find(cookie => cookie.includes(key))
-    if (targetCookie) {
-      const [_, value] = targetCookie.split("=")
-      return value
-    }
-  }
-
-  return null
-}
-
-export const onRequestGet = async ({ env, request }) => {
-  const cookies = request.headers.get('Cookie');
-  const token = getCookie(cookies, 'authToken');
-
-  try {
-    const { payload } = await jose.jwtVerify(token, await jose.importSPKI(env.AUTH_JWT_PUBLIC_KEY, 'ES384'), {
-      issuer: env.AUTH_JWT_ISSUER,
-      audience: env.AUTH_JWT_AUDIENCE,
-    });
-
-    return new Response(JSON.stringify({
-      payload
-    }));
-  }
-  catch {
-    return new Response(JSON.stringify(null));
-  }
-}
-
-
+export const onRequestGet = async ({ data }) => {
+  return new Response(JSON.stringify(data.auth));
+};
